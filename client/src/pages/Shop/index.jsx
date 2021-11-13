6682a73716cea679b17332f0ec451c6be230a4b0
chien <c11phamminh@gmail.com>
2021-11-13 15:01:21 +0700

    init

---
+++ client/src/pages/Shop/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+function Shop(props) {
+    return (
+        <div>
+            <h1>shoppppp</h1>
+            <Link to='/home' >Back</Link>
+        </div>
+    );
+}
+
+export default Shop;
